5cab2b79afcb33c0eda72e55fe40433372fa9731
Coline <coline.albano@etu.univ-amu.fr>
2025-12-17 11:16:34 +0100

    ajout de test_add

--- python-test-calculator/src/calulator.py
+++ python-test-calculator/src/calulator.py
@@ -15,12 +15,3 @@ def checkInputs(a, b):
 	if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
 		raise TypeError("Inputs must be either int or float!")
 
-#test_addition.py
-from src.calculator import add
-import pytest
-def test_add():
-	result = add(3,4)
-	assert restult ==7
-def test_add_string():
-	with pytest.raises(TypesError):
-		add("string",4)
